ccf74a7d0c9f7061473480644ed8a5b9dfdbe1f8
Christian Boltz <apparmor@cboltz.de>
2025-06-15 23:23:03 +0200

    Unify get_directory_contents(), and delete unused copy
    Copy the optimized version from common.py to easyprof.py (shouldn't
    change the behaviour).
    
    Since get_directory_contents() is only used in easyprof.py, delete the
    unused copy from common.py.

--- utils/apparmor/easyprof.py
+++ utils/apparmor/easyprof.py
@@ -200,12 +200,7 @@ def get_directory_contents(path):
     if not valid_path(path):
         return None
 
-    files = []
-    for f in glob.glob(path + "/*"):
-        files.append(f)
-
-    files.sort()
-    return files
+    return sorted(glob.glob(path + "/*"))
 
 
 def verify_policy(policy, exe, base=None, include=None):
