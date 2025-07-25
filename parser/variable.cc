gl1tch0x1/nu11s3c-zta

commit dfbd2dc4b172b7b04119de02626699fb2b3f55b0

parser: refactor variables and symbols table into their own class
While symtab for now has only static members, it will allow for a
change in the future for each profile to have their own symbols like
profile_name, etc.

parser/variable.cc
@@ -1,0 +1,355 @@
+/*
+ *   Copyright (c) 2025
+ *   Canonical Ltd. (All rights reserved)
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of version 2 of the GNU General Public
+ *   License published by the Free Software Foundation.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, contact Canonical Ltd.
+ */
+
+#include <cctype>
+#include <list>
+#include <tuple>
+
+#include "variable.h"
+#include "symtab.h"
+
+variable::variable(const char *var_name, struct value_list *values):
+	type(sd_set),
+	var_name(var_name)
+{
+	struct value_list *entry = NULL;
+	if (!values || !values->value) {
+		yyerror("Assert: valuelist returned NULL");
+	}
+	PDEBUG("Matched: set assignment for (%s)\n", var_name);
+
+	list_for_each(values, entry) {
+		this->values.insert(entry->value);
+	}
+}
+
+variable::variable(const char *var_name, const char *value):
+	type(sd_set),
+	var_name(var_name)
+{
+	PDEBUG("Matched: set assignment for (%s)\n", var_name);
+	this->values.insert(value);
+}
+
+variable::variable(const char *var_name, int boolean):
+	type(sd_boolean),
+	var_name(var_name),
+	boolean(boolean)
+{
+	PDEBUG("Matched: boolean assignment (%s) to %d\n", var_name, boolean);
+}
+
+char *variable::process_var(const char *var)
+{
+	const char *orig = var;
+	const char *valid;
+	int len = strlen(var);
+	int i;
+
+	if (*orig == '@' || *orig == '$') {
+		orig++;
+		len--;
+	} else {
+		PERROR("ASSERT: Found var '%s' without variable prefix\n",
+		       var);
+		return NULL;
+	}
+
+	if (*orig == '{') {
+		orig++;
+		len--;
+		if (orig[len - 1] != '}') {
+			PERROR("ASSERT: No matching '}' in variable '%s'\n",
+			       var);
+			return NULL;
+		} else
+			len--;
+	}
+
+	valid = orig;
+	for (i = 0; i < len; i++) {
+		/* first character must be alpha */
+		if (valid[i] == *orig) {
+			if (!isalpha(valid[i])) {
+				PERROR("Variable '%s' must start with alphabet letters\n",
+				       var);
+				return NULL;
+			}
+		} else if (!(valid[i] == '_' || isalnum(valid[i]))) {
+			PERROR("Variable '%s' contains invalid characters\n",
+			       var);
+			return NULL;
+		}
+	}
+
+	return strndup(orig, len);
+}
+
+int variable::add_set_value(struct value_list *values)
+{
+	struct value_list *entry = NULL;
+	PDEBUG("Matched: additive assignment for (%s)\n", var_name.c_str());
+
+	if (type != sd_set) {
+		PERROR("Variable %s is not a set variable\n", var_name.c_str());
+		return 2;
+	}
+
+	list_for_each(values, entry) {
+		this->values.insert(entry->value);
+	}
+	return 0;
+}
+
+std::tuple<std::string, std::string, std::string> extract_variable(const std::string& input)
+{
+	std::string prefix = "";
+	std::string variable = "";
+	std::string suffix = "";
+
+	ssize_t start_pattern_pos = -1;
+	ssize_t end_pattern_pos = -1;
+	size_t var_len;
+
+	bool bEscape = false;
+	size_t i;
+
+	for (i = 0; i < input.size(); i++) {
+		switch(input[i]) {
+		case '\\':
+			bEscape = !bEscape;
+			break;
+		case '@':
+			if (bEscape) {
+				bEscape = false;
+			} else if (input[i + 1] == '{') {
+				start_pattern_pos = i;
+				i += 2;
+			}
+			break;
+		case '}':
+			if (bEscape) {
+				bEscape = false;
+			} else if (start_pattern_pos != -1) {
+				end_pattern_pos = i;
+				goto found;
+			}
+			break;
+		default:
+			bEscape = false;
+			break;
+		}
+	}
+
+found:
+	if (start_pattern_pos == -1 || end_pattern_pos == -1) {
+		return std::make_tuple(prefix, variable, suffix); // "@{" or '}' not found
+	}
+
+	var_len = end_pattern_pos - start_pattern_pos + 1;
+
+	prefix = input.substr(0, start_pattern_pos);
+	variable = input.substr(start_pattern_pos, var_len);
+	suffix = input.substr(end_pattern_pos + 1);
+
+	return std::make_tuple(prefix, variable, suffix);
+}
+
+static void trim_leading_slash(std::string& str)
+{
+	std::size_t found = str.find_first_not_of('/');
+	if (found != std::string::npos)
+		str.erase(0, found);
+	else
+		str.clear(); // str is all '/'
+}
+
+static void trim_trailing_slash(std::string& str)
+{
+	std::size_t found = str.find_last_not_of('/');
+	if (found != std::string::npos)
+		str.erase(found + 1);
+	else
+		str.clear(); // str is all '/'
+}
+
+int variable::expand_by_alternation(char **name)
+{
+	std::string expanded_name = "";
+	bool filter_leading_slash = false;
+	bool filter_trailing_slash = false;
+
+	if (!name) {
+		PERROR("ASSERT: name to be expanded cannot be NULL\n");
+		exit(1);
+	}
+	if (!*name)		/* can happen when entry is optional */
+		return 0;
+
+	auto result = extract_variable(*name);
+	std::string prefix = std::get<0>(result);
+	std::string var = std::get<1>(result);
+	std::string suffix = std::get<2>(result);
+
+	if (prefix.empty() && var.empty() && suffix.empty()) {
+		return 0; /* no var found, name is unchanged */
+	}
+
+	free(*name);
+
+	if (!prefix.empty() && prefix[prefix.size() - 1] == '/') {
+		/* make sure to not collapse / in the beginning of the path */
+		std::size_t found = prefix.find_first_not_of('/');
+		if (found != std::string::npos)
+			filter_leading_slash = true;
+	}
+	if (!suffix.empty() && suffix[0] == '/')
+		filter_trailing_slash = true;
+
+	variable *ref = symtab::get_set_var(var.c_str());
+	if (!ref) {
+		PERROR("Failed to find declaration for: %s\n", var.c_str());
+		return 1;
+	}
+
+	size_t setsize = ref->expanded.size();
+	auto i = ref->expanded.begin();
+
+	if (setsize > 1) {
+		expanded_name += "{";
+	}
+
+	do {
+		std::string s = *i;
+		if (filter_leading_slash)
+			trim_leading_slash(s);
+		if (filter_trailing_slash)
+			trim_trailing_slash(s);
+
+		if (i != ref->expanded.begin()) {
+			expanded_name += ",";
+		}
+
+		expanded_name += s;
+	} while (++i != ref->expanded.end());
+
+	if (setsize > 1) {
+		expanded_name += "}";
+	}
+
+	expanded_name = prefix + expanded_name + suffix;
+	*name = strdup(expanded_name.c_str());
+	if (!*name) {
+		errno = ENOMEM;
+		return -1;
+	}
+	/* recursive until no variables are found in *name */
+	return expand_by_alternation(name);
+}
+
+int variable::expand_variable()
+{
+	int rc = 0;
+
+	if (type == sd_boolean) {
+		PERROR("Referenced variable %s is a boolean used in set context\n",
+		       var_name.c_str());
+		return 2;
+	}
+
+	/* already done */
+	if (!expanded.empty())
+		return 0;
+
+	expanding = true;
+
+	std::list<std::string> work_set(values.begin(), values.end());
+	for (auto value : work_set) {
+		auto result = extract_variable(value);
+		std::string prefix = std::get<0>(result);
+		std::string var = std::get<1>(result);
+		std::string suffix = std::get<2>(result);
+
+		if (prefix.empty() && var.empty() && suffix.empty()) {
+			expanded.insert(value); /* no var left to expand */
+			continue;
+		}
+		char *name = variable::process_var(var.c_str());
+		variable *ref = symtab::lookup_existing_symbol(name);
+		if (!ref) {
+			PERROR("Failed to find declaration for: %s\n", var.c_str());
+			rc = 1;
+			goto out;
+		}
+		if (ref->expanding) {
+			PERROR("Variable @{%s} is referenced recursively (by @{%s})\n",
+			       ref->var_name.c_str(), var_name.c_str());
+			rc = 1;
+			goto out;
+		}
+		rc = ref->expand_variable();
+		if (rc != 0) {
+			goto out;
+		}
+
+		if (ref->expanded.empty()) {
+			PERROR("ASSERT: Variable @{%s} should have been expanded but isn't\n",
+			       ref->var_name.c_str());
+			exit(1);
+		}
+		for (auto refvalue : ref->expanded) {
+			/* there could still be vars in suffix, so add
+			 * to work_set, not expanded */
+			work_set.push_back(prefix + refvalue + suffix);
+		}
+	}
+
+out:
+	expanding = false;
+	return rc;
+}
+
+void variable::dump_set_values(std::set<std::string> values)
+{
+	for (auto value : values)
+		printf(" \"%s\"", value.c_str());
+}
+
+void variable::dump(bool do_expanded)
+{
+	switch(type) {
+	case sd_boolean:
+		printf("$%s = %s\n", var_name.c_str(),
+		       boolean ?  "true" : "false");
+		break;
+	case sd_set:
+		printf("@%s =", var_name.c_str());
+		if (do_expanded) {
+			if (expanded.empty()) {
+				expand_variable();
+			}
+			dump_set_values(expanded);
+		} else {
+			dump_set_values(values);
+		}
+		printf("\n");
+		break;
+	default:
+		PERROR("ASSERT: unknown symbol table type for %s\n", var_name.c_str());
+		exit(1);
+	}
+}

parser/Makefile
@@ -105,12 +105,12 @@ SRCS = parser_common.c parser_include.c parser_interface.c parser_lex.c \
        parser_alias.c common_optarg.c lib.c network.cc \
        mount.cc dbus.cc profile.cc rule.cc signal.cc ptrace.cc \
        af_rule.cc af_unix.cc policy_cache.c default_features.c userns.cc \
-       mqueue.cc io_uring.cc all_rule.cc cond_expr.cc
+       mqueue.cc io_uring.cc all_rule.cc cond_expr.cc variable.cc symtab.cc
 STATIC_HDRS = af_rule.h af_unix.h capability.h common_optarg.h dbus.h \
        file_cache.h immunix.h lib.h mount.h network.h parser.h \
        parser_include.h parser_version.h policy_cache.h policydb.h \
        profile.h ptrace.h rule.h signal.h userns.h mqueue.h io_uring.h \
-       common_flags.h bignum.h all_rule.h cond_expr.h
+       common_flags.h bignum.h all_rule.h cond_expr.h variable.h symtab.h
 
 SPECIAL_HDRS = parser_yacc.h unit_test.h base_cap_names.h
 GENERATED_HDRS = af_names.h generated_af_names.h \
@@ -330,6 +330,13 @@ all_rule.o: all_rule.cc $(HDRS)
 
 cond_expr.o: cond_expr.cc $(HDRS)
 	$(CXX) $(EXTRA_CFLAGS) -c -o $@ $<
+
+variable.o: variable.cc $(HDRS)
+	$(CXX) $(EXTRA_CFLAGS) -c -o $@ $<
+
+symtab.o: symtab.cc $(HDRS)
+	$(CXX) $(EXTRA_CFLAGS) -c -o $@ $<
+
 
 parser_version.h: Makefile
 	@echo \#define PARSER_VERSION \"$(VERSION)\" > .ver

parser/cond_expr.cc
@@ -18,6 +18,7 @@
 
 #include "cond_expr.h"
 #include "parser.h"
+#include "symtab.h"
 
 cond_expr::cond_expr(bool result):
 	result(result)
@@ -26,20 +27,21 @@ cond_expr::cond_expr(bool result):
 
 cond_expr::cond_expr(const char *var, bool defined)
 {
-	char *var_name = process_var(var);
-
+	variable *ref;
 	if (!defined) {
-		int ret = get_boolean_var(var_name);
-		if (ret < 0) {
+		ref = symtab::get_boolean_var(var);
+		if (!ref) {
 			/* FIXME check for set var */
-			free(var_name);
 			yyerror(_("Unset boolean variable %s used in if-expression"), var);
 		}
-		result = ret;
+		result = ref->boolean;
 	} else {
-		void *set_value = get_set_var(var_name);
-		PDEBUG("Matched: defined set expr %s value %lx\n", var_name, (long) set_value);
-		result = !! (long) set_value;
+		ref = symtab::get_set_var(var);
+		if (!ref) {
+			result = false;
+		} else {
+			PDEBUG("Matched: defined set expr %s value %s\n", var, ref->expanded.begin()->c_str());
+			result = true;
+		}
 	}
-	free(var_name);
 }

parser/parser.h
@@ -415,8 +415,6 @@ extern const char *local_name(const char *name);
 /* parser_variable.c */
 int expand_entry_variables(char **name);
 extern int process_variables(Profile *prof);
-extern struct var_string *split_out_var(const char *string);
-extern void free_var_string(struct var_string *var);
 
 /* parser_misc.c */
 extern void warn_uppercase(void);
@@ -457,38 +455,6 @@ bool entry_add_prefix(struct cod_entry *entry, const prefixes &p, const char *&e
 
 #define SECONDS_P_MS (1000LL * 1000LL)
 long long convert_time_units(long long value, long long base, const char *units);
-
-
-/* parser_symtab.c */
-struct set_value {
-	char *val;
-	struct set_value *next;
-};
-enum var_type {
-	sd_boolean,
-	sd_set,
-};
-
-struct symtab {
-	char *var_name;
-	enum var_type type;
-	int boolean;
-	struct set_value *values;
-	struct set_value *expanded;
-};
-
-extern int add_boolean_var(const char *var, int boolean);
-extern int get_boolean_var(const char *var);
-extern int new_set_var(const char *var, const char *value);
-extern int add_set_value(const char *var, const char *value);
-extern struct set_value *get_set_var(const char *var);
-extern char *get_next_set_value(struct set_value **context);
-extern int insert_set_var(struct symtab *var);
-extern struct symtab *remove_set_var(const char *var_name);
-extern int delete_set_var(const char *var_name);
-extern void dump_symtab(void);
-extern void dump_expanded_symtab(void);
-void free_symtabs(void);
 
 /* parser_alias.c */
 extern int new_alias(const char *from, const char *to);

parser/parser_main.c
@@ -51,6 +51,7 @@
 #include "policy_cache.h"
 #include "libapparmor_re/apparmor_re.h"
 #include "file_cache.h"
+#include "symtab.h"
 
 #define OLD_MODULE_NAME "subdomain"
 #define PROC_MODULES "/proc/modules"
@@ -1092,7 +1093,7 @@ void reset_parser(const char *filename)
 	memset(&cache_tstamp, 0, sizeof(cache_tstamp));
 	mru_skip_cache = 1;
 	free_aliases();
-	free_symtabs();
+	symtab::free_symtab();
 	free_policies();
 	reset_include_stack(filename);
 	aa_features_unref(policy_features);
@@ -1223,7 +1224,7 @@ int process_profile(int option, aa_kernel_interface *kernel_interface,
 	}
 
 	if (dump_vars) {
-		dump_symtab();
+		symtab::dump(false);
 		goto out;
 	}
 
@@ -1234,7 +1235,7 @@ int process_profile(int option, aa_kernel_interface *kernel_interface,
   	}
 
 	if (dump_expanded_vars) {
-		dump_expanded_symtab();
+		symtab::dump(true);
 		goto out;
 	}
 

parser/parser_misc.c
@@ -532,35 +532,6 @@ char *processid(const char *string, int len)
 	return processunquoted(string, len);
 }
 
-/* strip off surrounding delimiters around variables */
-char *process_var(const char *var)
-{
-	const char *orig = var;
-	int len = strlen(var);
-
-	if (*orig == '@' || *orig == '$') {
-		orig++;
-		len--;
-	} else {
-		PERROR("ASSERT: Found var '%s' without variable prefix\n",
-		       var);
-		return NULL;
-	}
-
-	if (*orig == '{') {
-		orig++;
-		len--;
-		if (orig[len - 1] != '}') {
-			PERROR("ASSERT: No matching '}' in variable '%s'\n",
-		       		var);
-			return NULL;
-		} else
-			len--;
-	}
-
-	return strndup(orig, len);
-}
-
 /* returns -1 if value != true or false, otherwise 0 == false, 1 == true */
 int str_to_boolean(const char *value)
 {

commit 6673be07aabdf7961551ad2ee07abb7aaf02d316

parser/variables: fix read-after-free in error case
When variable expansion occurs, the expansion attempts to replace the
memory location of the string containing the variable, and frees the
string it is replacing. However, this occurs before the variable lookup
occurs to determine if there is an appropriate declaration for the
variable. When the failing expansion occurs in a profile name, this
causes a read-after-free (followed by a double free) because the error
handling path attempts to report the profile name in the error message.

This can be reproduced like so, using the
tst/simple_tests/vars/vars_profile_name_23.sd testcase:

```
$ ../apparmor_parser --config-file=./parser.conf -M features_files/features.all -S -I /home/sbeattie/git/apparmor/parser/tst/./simple_tests/ ./simple_tests/vars/vars_profile_name_23.sd
Failed to find declaration for: @{FOO}
ERROR expanding variables for profile #xQV, failed to load
free(): double free detected in tcache 2
```

Fix this by waiting to free the profile name field until after the
variable declaration has successfully been looked up. This results in
the test case reporting the following error:

```
$ ../apparmor_parser --config-file=./parser.conf -M features_files/features.all -S -I /home/sbeattie/git/apparmor/parser/tst/./simple_tests/ ./simple_tests/vars/vars_profile_name_23.sd
Failed to find declaration for: @{FOO}
ERROR expanding variables for profile /does/not/exist@{FOO}, failed to load

```

Fixes: dfbd2dc4b ("parser: refactor variables and symbols table into their own class")
Signed-off-by: Steve Beattie <steve@nxnw.org>
Ref: https://gitlab.com/apparmor/apparmor/-/merge_requests/1747

parser/variable.cc
@@ -211,8 +211,6 @@ int variable::expand_by_alternation(char **name)
 		return 0; /* no var found, name is unchanged */
 	}
 
-	free(*name);
-
 	if (!prefix.empty() && prefix[prefix.size() - 1] == '/') {
 		/* make sure to not collapse / in the beginning of the path */
 		std::size_t found = prefix.find_first_not_of('/');
@@ -227,6 +225,8 @@ int variable::expand_by_alternation(char **name)
 		PERROR("Failed to find declaration for: %s\n", var.c_str());
 		return 1;
 	}
+
+	free(*name);
 
 	size_t setsize = ref->expanded.size();
 	auto i = ref->expanded.begin();